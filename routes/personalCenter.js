08cc3378a9a7c841f034c7e4f929a870510564ba
jellyfishgh <1129088994@qq.com>
2016-06-01 01:27:50 +0800

    my personalCenter and other's personalCenter

--- routes/personalCenter.js
+++ routes/personalCenter.js
@@ -9,18 +9,27 @@ router.get('/:username', function(req, res, next) {
             username: uname
         }, function(err, users) {
             if (err || users.length === 0) {
-                res.render('personalInfoReg', {
-                    title: uname + '需要完善个人信息',
-                    name: uname
-                });
+                if (uname === req.session.username) {
+                    res.render('personalInfoReg', {
+                        title: uname + '需要完善个人信息',
+                        name: uname
+                    });
+                } else {
+
+                }
             } else {
-                res.render('personalCenter', {
-                    userInfo: users[0]
-                });
+                if (uname === req.session.username) {
+                    res.render('personalCenter', {
+                        userInfo: users[0]
+                    });
+                } else {
+
+                }
             }
         });
     } else {
-        res.redirect('../login');
+        // 登录后仍然访问原来想要访问的url        
+        res.redirect('../login/:' + req.href);
     }
 });
 
